1857a0987af4f496e1c0d414199204dda2139d63
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-07-25 21:24:59 +0530

    description testimonial generatebtn footer section added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import BuyCredit from './pages/BuyCredit'
 import Home from './pages/Home'
 import Result from './pages/Result'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path='/result'  element={<Result/>}/>
         <Route path='/buy'  element={<BuyCredit/>}/>
       </Routes>
+      <Footer/>
       
     </div>
   )

f2aa1736c0c3c7e536f6c70d2db33beaaac1d756
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-07-25 19:45:04 +0530

    home page added

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This scans all files in src/ with these extensions
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

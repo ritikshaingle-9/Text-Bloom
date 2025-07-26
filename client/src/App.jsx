7c9eab9bdb1bdf713917189f6d26e2585e6be107
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-07-26 18:21:35 +0530

    buy and sign up page made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Routes , Route } from 'react-router-dom'
 import './index.css';
 
@@ -7,12 +7,17 @@ import Home from './pages/Home'
 import Result from './pages/Result'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer';
+import Login from './components/Login';
+import { AppContext } from './context/AppContext';
 
 const App = () => {
+
+  const {showLogin} = useContext(AppContext)
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
 
      <Navbar/>
+     {showLogin && <Login/>}
       <Routes>
         <Route path='/'  element={<Home/>}/>
         <Route path='/result'  element={<Result/>}/>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,10 +5,13 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props)=>{
     const [user,setUser] = useState(null);
+    const [showLogin, setShowLogin] = useState(false);
+
 
     const value = {
-        user,setUser
+        user,setUser,showLogin,setShowLogin
     }
+    
     return(
         <AppContext.Provider value={value}>
             {props.children}
